10a82a5564a980b8d14d7113b896cfe592203bd7
kecspeter <kecskespeti@gmail.com>
2020-05-14 19:40:50 +0200

    - added to frametime counter

--- src/main/kotlin/RegionData.kt
+++ src/main/kotlin/RegionData.kt
@@ -5,7 +5,8 @@ class RegionData(
         var InnerStartPos: Vector2D,        //Mandel/Julia-SetStartPos
         var InnerZoom: Double,              //current zoom to offset the position and get iterator in region
         var type: Int,                      //Mandelbrot or Julia
-        var screen: ScreenImageHandler      //the source screen, to copy the region
+        var screen: ScreenImageHandler,     //the source screen, to copy the region
+        var frameTime: Long                 //time to render Region
     )
 {
 }
