45df44248df777cca12b110c1656e987708ff001
kecspeter <kecskespeti@gmail.com>
2020-05-14 19:43:07 +0200

    - added benchmark data writer

---
+++ src/main/kotlin/BenchmarkWriter.kt
@@ -0,0 +1,32 @@
+import kotlinx.coroutines.internal.artificialFrame
+import java.io.File
+
+class BenchmarkWriter
+{
+    var mode = 0
+    var testData = mutableListOf<String>()
+
+    fun writeOut(name: String)
+    {
+        File("$name.txt").bufferedWriter().use { out ->
+            testData.forEach {
+                out.write("${it}\n")
+            }
+            //out.close()
+        }
+    }
+
+    private fun prepareData(data: MutableList<Long>) : String
+    {
+        var line = ""
+        for (frameTime in data)
+        {
+            line += "$frameTime\t"
+        }
+        return line
+    }
+    fun testSession(data: MutableList<Long>, sessionID: String)
+    {
+        testData.add("$sessionID\t${prepareData(data)}")
+    }
+}
