e35b68dbcced71f1ceda9df86493c6b93dbcdc1e
kecspeter <kecskespeti@gmail.com>
2020-05-22 19:04:58 +0200

    package change

---
+++ src/main/kotlin/data/RegionData.kt
@@ -0,0 +1,13 @@
+package data
+
+import drawing.ScreenImageHandler
+
+class RegionData(
+        var RegionStartPos: Vector2D,       //MainScreenStartPos
+        var RegionSize: Vector2D,           //ChunkStartPos
+        var InnerStartPos: Vector2D,        //ChunkSize
+        var InnerZoom: Double,              //Mandel/Julia-SetStartPos
+        var type: Int,                      //current zoom to offset the position and get iterator in region
+        var screen: ScreenImageHandler,     //Mandelbrot or Julia
+        var frameTime: Long                 //time to render Region){}
+)
