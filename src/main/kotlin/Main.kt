132ffe59c8fd50cce8098fd5892faea81a0bf6b1
kecspeter <kecskespeti@gmail.com>
2020-05-22 19:04:36 +0200

    - app uses launch parameters now

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,30 @@
+import data.Vector2D
+import drawing.AppWindow
 import javafx.application.Application
+import java.lang.IndexOutOfBoundsException
 
 fun main(args: Array<String>)
 {
+    if(args.isNotEmpty())
+    {
+        try
+        {
+            AppWindow.startWidth = args[0].toInt()
+            AppWindow.startHeight = args[1].toInt()
+            AppWindow.startMaxThread = args[2].toInt()
+            AppWindow.startChunkSize = Vector2D(args[3].toDouble(), args[4].toDouble())
+        }
+        catch (e: IndexOutOfBoundsException)
+        {
+            println("Wrong parameters!\n" +
+                    "Using default parameters: 960 576 16 96 96\n" +
+                    "Usage: 'WIDTH' 'HEIGHT' 'MaxThreadCount' 'ChunkSizeX' 'ChunkSizeY'")
+            AppWindow.startWidth = 960
+            AppWindow.startHeight = 576
+            AppWindow.startMaxThread = 16
+            AppWindow.startChunkSize = Vector2D(64.0, 96.0)
+        }
+    }
+
     Application.launch(AppWindow::class.java, *args)
 }
